16e9b6b03ef2cf3ecb5812cbd35e851ae6d0a3ba
In Niko <inniko9111@gmail.com>
2022-01-30 17:12:54 +0200

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function () {
+    player.getCurrentTime().then(function (sec) {
+      console.log(`Current time:`, sec);
+      localStorage.setItem('videoplayer-current-time', JSON.stringify(sec));
+    });
+  }, 1000),
+);
+
+const timeToStart = localStorage.getItem('videoplayer-current-time') || 0;
+const parseData = JSON.parse(timeToStart);
+console.log(parseData);
+player.setCurrentTime(parseData);
